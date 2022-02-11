8cfbbd804c1d375df2e9e55ce60c42614f7bb01d
den2654 <den26544@mail.ru>
2022-02-11 15:10:44 +0300

    push

---
+++ DZ_SkillBox_5.5_Method/DZ_SkillBox_5.5_Method/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+static class Program
+{
+    private static void Main()
+    {
+        //string text = "простой текст без точек и запятых";
+        Console.Write("Введите предложение: ");
+        string text = Console.ReadLine();
+        WordWrap(text, true);
+        ReversWords(text);
+    }
+
+    private static string[] WordWrap(string word, bool isWrap)
+    {
+        if (isWrap)
+        {
+            var ArrayWords = word.Split(' ');
+            foreach (var item in ArrayWords)
+            {
+                Console.WriteLine(CultureInfo.CurrentCulture.TextInfo.ToTitleCase(item.ToLower()));
+            }
+
+            return ArrayWords;
+
+        }
+        else
+        {
+            return word.Split(' ');
+        }
+    }
+
+    private static void ReversWords(string words)
+    {
+        var ArrayWord = WordWrap(words, false);
+        var w = ArrayWord.Reverse();
+
+        foreach (var item in w)
+        {
+            Console.Write(item + " ");
+        }
+    }
+
+}
